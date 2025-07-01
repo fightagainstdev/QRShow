65a502421a6693a100eb52b6d07569cda4d98fcc
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-07-01 16:15:05 +0800

    Update LikedPosts.tsx

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -14,7 +14,7 @@ const LikedPosts = () => {
   return (
     <>
       {currentUser.liked.length === 0 && (
-        <p className="text-light-4">No liked posts</p>
+        <p className="text-light-4">没有喜欢的帖子</p>
       )}
 
       <GridPostList posts={currentUser.liked} showStats={false} />
