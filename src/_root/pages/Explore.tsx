340fa40b05a74e77cc3bf2e731b43c8d674ffff2
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-07-01 16:14:23 +0800

    Update Explore.tsx

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -18,7 +18,7 @@ const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) =
     return <GridPostList posts={searchedPosts.documents} />;
   } else {
     return (
-      <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+      <p className="text-light-4 mt-10 text-center w-full">无结果</p>
     );
   }
 };
@@ -73,10 +73,10 @@ const Explore = () => {
       </div>
 
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+        <h3 className="body-bold md:h3-bold">今日热门</h3>
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          <p className="small-medium md:base-medium text-light-2">All</p>
+          <p className="small-medium md:base-medium text-light-2">全部</p>
           <img
             src="/assets/icons/filter.svg"
             width={20}
